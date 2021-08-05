9f367347ed4acf91ace01499fb31bdf59d509f44
Will <wjbeshore@gmail.com>
2021-08-05 12:59:00 -0500

    PLaying around with Javascript in html

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import "./styles.css";
-
+let name = "Will";
 export default function App() {
   return (
     <div className="App">
       <h1>Test</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h2>My name is {name + " Beshore"}!</h2>
     </div>
   );
 }
