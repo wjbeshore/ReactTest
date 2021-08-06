af8c10312c7947d5707c6d0f8c7123f8d0babc97
Will <wjbeshore@gmail.com>
2021-08-06 14:54:39 -0500

    Played around with style and logic

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import "./styles.css";
 let name = "Will";
+let style = {
+  color: "Orange"
+};
+
+var d = new Date();
+var n = d.getHours();
+let time;
+let tod = "AM";
+
+if (n > 12) {
+  time = n - 12;
+  tod = "PM";
+  style.color = "Blue";
+}
 export default function App() {
   return (
     <div className="App">
       <h1>Test</h1>
-      <h2>My name is {name + " Beshore"}!</h2>
+      <h2 style={style}>The time is {time + " " + tod}</h2>
     </div>
   );
 }
